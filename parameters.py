8830b4dbbe2f3482bb308169edff909ea701b88f
nxbdrswpxc-ship-it <nxbdrswpxc@privaterelay.appleid.com>
2025-12-23 14:05:19 +0000

    Update parameters.py

--- parameters.py
+++ parameters.py
@@ -1,1 +1,3 @@
+# Below are three equations for the model, as well as the parameters that will be defined in this earlier version all Parameters are defined and then the equations are layed out in fucntions below
 
+Is_equation = 
